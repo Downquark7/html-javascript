26bc8737b409e74387dc1475775bef2bee3322b4
Downquark7 <mail2emn@gmail.com>
2016-04-28 10:06:36 -0500

    Update rainbow-background.js

--- rainbow-background.js
+++ rainbow-background.js
@@ -12,8 +12,8 @@ document.getElementById("progressb").value=”75”;
 document.getElementById("progressb").max=”255”;
 
 function byte2Hex(n) {
-    var nybHexString = "0123456789ABCDEF";
-    return String(nybHexString.substr((n >> 4) & 0x0F, 1)) + nybHexString.substr(n & 0x0F, 1);
+  var nybHexString = "0123456789ABCDEF";
+  return String(nybHexString.substr((n >> 4) & 0x0F, 1)) + nybHexString.substr(n & 0x0F, 1);
 }
 var rrrrr = 1;
 var ggggg = 1;
@@ -21,18 +21,18 @@ var bbbbb = 1;
 var seqqqqq = 1;
 
 function changecolorrrrr() {
-document.body.style.backgroundColor = "#" + byte2Hex(rrrrr) + byte2Hex(ggggg) + byte2Hex(bbbbbb);
-document.title = "#" + byte2Hex(rrrrrr) + byte2Hex(ggggg) + byte2Hex(bbbbbb);
-document.getElementById("progressr").value = rrrrrr;
-document.getElementById("progressg").value = ggggg;
-document.getElementById("progressb").value = bbbbb;
+  document.body.style.backgroundColor = "#" + byte2Hex(rrrrr) + byte2Hex(ggggg) + byte2Hex(bbbbbb);
+  document.title = "#" + byte2Hex(rrrrrr) + byte2Hex(ggggg) + byte2Hex(bbbbbb);
+  document.getElementById("progressr").value = rrrrrr;
+  document.getElementById("progressg").value = ggggg;
+  document.getElementById("progressb").value = bbbbb;
 }
 function changeeeee() {
-    var frequencyyyyy = 0.001;
-    rrrrr = Math.sin(frequencyyyyy * seqqqqq) * 128 + 127;
-    bbbbb = Math.sin(frequencyyyyy * seqqqqq + 2 * Math.PI / 3) * 128 + 127;
-    ggggg = Math.sin(frequencyyyyy * seqqqqq + 4 * Math.PI / 3) * 128 + 127;
-    seqqqqq = seqqqqq + 1;
-    changecolorrrrr();
+  var frequencyyyyy = 0.001;
+  rrrrr = Math.sin(frequencyyyyy * seqqqqq) * 128 + 127;
+  bbbbb = Math.sin(frequencyyyyy * seqqqqq + 2 * Math.PI / 3) * 128 + 127;
+  ggggg = Math.sin(frequencyyyyy * seqqqqq + 4 * Math.PI / 3) * 128 + 127;
+  seqqqqq = seqqqqq + 1;
+  changecolorrrrr();
 };
 setInterval('changeeeee()', 40);
