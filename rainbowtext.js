d5ea61ef86e1e00d32de784dd8bc45028b9fc041
Downquark7 <mail2emn@gmail.com>
2016-04-28 10:01:04 -0500

    Update rainbowtext.js

--- rainbowtext.js
+++ rainbowtext.js
@@ -21,7 +21,7 @@ for (var i = 0; i < len; ++i)
 	var rd = Math.sin(freq*i + 0 * Math.PI / 3) * width + center;
 	var gn = Math.sin(freq*i + 2 * Math.PI / 3) * width + center;
 	var bl = Math.sin(freq*i + 4 * Math.PI / 3) * width + center;
-	var rainbownode = document.createElement("div");
+	var rainbownode = document.createElement("font");
   rainbownode.innerHTML = rainbowtextstring;
   rainbownode.style = "font-size: "+fontsizepx+"; color: "+RGB2Color(rd,gn,bl)+";";
   document.body.appendChild(rainbownode);
