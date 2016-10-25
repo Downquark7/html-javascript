376071ab2fe8c6b35345843c51a8fec95a200926
Downquark7 <mail2emn@gmail.com>
2016-10-25 13:50:55 -0500

    Update rainbowtext.js

--- rainbowtext.js
+++ rainbowtext.js
@@ -1,4 +1,5 @@
-function byte2Hex(n)
+function addRainbow(object) {
+  function byte2Hex(n)
 {
   var nybHexString = "0123456789ABCDEF";
   return String(nybHexString.substr((n >> 4) & 0x0F,1)) + nybHexString.substr(n & 0x0F,1);
@@ -8,21 +9,21 @@ function RGB2Color(r,g,b)
 {
   return '#' + byte2Hex(r) + byte2Hex(g) + byte2Hex(b);
 }
-
-var center = 128;
-var width = 127;
-var len = 100;
-var freq = 2*Math.PI/len;
-var fontsizepx = "16";
-var rainbowtextstring = "Hello World! ";
-
-for (var i = 0; i < len; ++i)
+  var center = 128;
+  var width = 127;
+  var rainbowtextstring = object.innerText;
+  object.innerText = "";
+  var len = rainbowtextstring.length;
+  var freq = 2*Math.PI/len;
+  
+for (var i = 0; i < len; i++)
 {
   var rd = Math.sin(freq*i + 0 * Math.PI / 3) * width + center;
   var gn = Math.sin(freq*i + 2 * Math.PI / 3) * width + center;
   var bl = Math.sin(freq*i + 4 * Math.PI / 3) * width + center;
   var rainbownode = document.createElement("font");
-  rainbownode.innerHTML = rainbowtextstring;
-  rainbownode.style = "font-size: "+fontsizepx+"; color: "+RGB2Color(rd,gn,bl)+";";
-  document.body.appendChild(rainbownode);
+  rainbownode.innerHTML = rainbowtextstring[i];
+  rainbownode.style = "color: "+RGB2Color(rd,gn,bl)+";";
+  object.appendChild(rainbownode);
+}
 }
